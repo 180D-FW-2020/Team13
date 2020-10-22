8c64d89dd3af5dbc0a06dae17c8b8cb374654d21
Anchal Sinha <43023676+anchalsinha@users.noreply.github.com>
2020-10-21 17:25:11 -0700

    Reorganizing and added shooting

---
+++ Zombie Shooter/Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public MouseControlType mouseControlType;
+
+    // reticle
+    public Transform reticle;
+
+    //camera
+    public Camera playerCamera;
+    public float cameraRotateSensitivity;
+
+    private Ray aimingRay;
+    private RaycastHit hit;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Aim();
+
+        if (Input.GetMouseButtonDown(0)) // click to shoot
+        {
+            Shoot();
+        }
+    }
+
+    private void Aim()
+    {
+        if (mouseControlType == MouseControlType.Reticle)
+        {
+            reticle.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+        }
+        else if (mouseControlType == MouseControlType.Camera)
+        {
+            playerCamera.transform.rotation *= Quaternion.Euler(-Input.GetAxis("Mouse Y") * cameraRotateSensitivity, Input.GetAxis("Mouse X") * cameraRotateSensitivity, 0);
+        }
+        aimingRay = playerCamera.ScreenPointToRay(reticle.position);
+    }
+
+    private void Shoot()
+    {
+        if (Physics.Raycast(aimingRay, out hit))
+        {
+            if (hit.transform.tag == "Enemy")
+            {
+                Debug.Log("GOTTEM");
+            }
+        }
+    }
+}
---
+++ Zombie Shooter/Assets/Scripts/Utilities/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public static class Constants
+{
+
+}
+
+public enum MouseControlType
+{
+    Reticle = 1,
+    Camera = 2
+}

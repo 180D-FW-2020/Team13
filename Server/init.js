f819f35e5c38fdf6a67b9cc844a1e014baed571e
Anchal Sinha <43023676+anchalsinha@users.noreply.github.com>
2021-01-11 15:42:11 -0800

    Update server

--- Server/init.js
+++ Server/init.js
@@ -8,7 +8,11 @@ const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
-const io = socketIO(server);
+const io = socketIO(server, {
+  cors: {
+    origin: '*',
+  }
+});
 
 
 module.exports = io;
--- Server/server.js
+++ Server/server.js
@@ -10,70 +10,67 @@ var connectedClients = {};
 var enemies = {};
 
 io.on('connection', socket => {
+    console.log("Client connected");
     socket.on('ping', (data) => {
         socket.emit('pong', data);
     });
- });
 
-io.on('ping', (data) => {
-    console.log('Game started');
-    io.emit('start', data);
+    socket.on('start', (data) => {
+        console.log('Game started');
+        io.emit('start', data);
+    });
+    
+    socket.on('register', (data) => {
+        console.log("Adding " + data.id);
+    
+        if (connectedClients.length() == 0){
+            initEnemies();
+        }
+    
+        let client = new Client(data.id);
+        connectedClients[data.id] = client;
+    
+        let init = {
+            playerList: Object.keys(connectedClients),
+            enemyPositions: enemies
+        }
+        io.emit('initialize', init);
+    });
+    
+    socket.on('leave', (data) => {
+        console.log("Removing " + data.id);
+        delete connectedClients[data.id];
+    
+        let leave = {
+            playerList: Object.keys(connectedClients)
+        }
+        io.emit('leave', leave);
+    });
+    
+    socket.on('enemyAttack', (data) => {
+        console.log("Enemy attacking " + data.id);
+        connectedClients[data.id].decrementHealth();
+    });
+    
+    socket.on('enemyShot', (data) => {
+        console.log("Enemy " + data.enemyId + " shot by " + data.id);
+        if (data.enemyId in enemies) {
+            console.log("Enemy " + data.enemyId + " killed by " + data.id);
+            io.emit('enemyKilled', data);
+            connectedClients[data.id].registerKill();
+        }
+    });
+    
+    socket.on('state', (data) => {
+        connectedClients[data.id].updateRotation(data.rotation);
+    })
 });
-
-io.on('start', (data) => {
-    console.log('Game started');
-    io.emit('start', data);
-});
-
-io.on('register', (data) => {
-    console.log("Adding " + data.id);
-
-    if (connectedClients.length() == 0){
-        initEnemies();
-    }
-
-    let client = new Client(data.id);
-    connectedClients[data.id] = client;
-
-    let init = {
-        playerList: Object.keys(connectedClients),
-        enemyPositions: enemies
-    }
-    io.emit('initialize', init);
-});
-
-io.on('leave', (data) => {
-    console.log("Removing " + data.id);
-    delete connectedClients[data.id];
-
-    let leave = {
-        playerList: Object.keys(connectedClients)
-    }
-    io.emit('leave', leave);
-});
-
-io.on('enemyAttack', (data) => {
-    console.log("Enemy attacking " + data.id);
-    connectedClients[data.id].decrementHealth();
-});
-
-io.on('enemyShot', (data) => {
-    console.log("Enemy " + data.enemyId + " shot by " + data.id);
-    if (data.enemyId in enemies) {
-        console.log("Enemy " + data.enemyId + " killed by " + data.id);
-        io.emit('enemyKilled', data);
-        connectedClients[data.id].registerKill();
-    }
-});
-
-io.on('state', (data) => {
-    connectedClients[data.id].updateRotation(data.rotation);
-})
 
 function initEnemies() {
     for (let i = 0; i < numEnemies; i++) {
         enemies[i] = "" + ((Math.floor(Math.random() * 11)-5)) * 4 + "," + (Math.floor(Math.random() * 6) * 4);
     }
+    console.log('Enemies initialized to: ' + JSON.stringify(enemies));
 }
 
 setInterval(function(){
